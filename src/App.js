31b7aeb187b3441d7e395a6e5a39162079e95e0c
Abdalluh28 <Abdok7496@gmail.com>
2025-03-15 11:49:03 +0200

    responsive edit

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ export default function App() {
             <footer className="bg-gray-800 text-white pl-5 py-6 shadow-md flex flex-col">
                 {/* Contributors */}
                 <div className="flex lg:flex-row flex-col">
-                    <div className="flex flex-row lg:w-1/2 w-full mb-4">
-                        <div className="mb-2 w-1/2">
+                    <div className="flex md:flex-row flex-col lg:w-1/2 w-full mb-4">
+                        <div className="mb-2 md:w-1/2 w-full">
                             <h2 className="text-lg font-semibold">Developed By</h2>
                             <p className="text-sm md:text-base pl-2 font-bold">Omnia Essam</p>
                             <p className="text-sm md:text-base pl-2 font-bold">Rokaya Ayman</p>
@@ -33,7 +33,7 @@ export default function App() {
                             <p className="text-sm md:text-base pl-2 font-bold">Mai Ali</p>
                         </div>
 
-                        <div className="mb-2 w-1/2">
+                        <div className="mb-2 md:w-1/2 w-full">
                             <h2 className="text-lg font-semibold">Contact</h2>
                             <p className="text-sm md:text-base pl-2">STEM-Chemistry2022_23@edu.asu.edu.eg</p>
                             <p className="text-sm md:text-base pl-2">STEM-Chemistry2022_5@edu.asu.edu.eg</p>
@@ -45,14 +45,14 @@ export default function App() {
                     </div>
 
                     {/* Supervisor */}
-                    <div className="flex flex-row lg:w-1/2 w-full mb-4">
-                        <div className="mb-2 w-1/2">
+                    <div className="flex md:flex-row flex-col lg:w-1/2 w-full mb-4">
+                        <div className="mb-2 md:w-1/2 w-full">
                             <h2 className="text-lg font-semibold">Supervisor</h2>
                             <p className="text-sm md:text-base pl-2">Dr. Raghda Kamal Elden</p>
                         </div>
 
                         {/* College/Institution */}
-                        <div className="mb-2 w-1/2">
+                        <div className="mb-2 md:w-1/2 w-full">
                             <h2 className="text-lg font-semibold">University</h2>
                             <p className="text-sm md:text-base pl-2">Ain Shams University</p>
                             <p className="text-sm md:text-base pl-2">Bachelor of Science and Education (STEM Chemistry, English)</p>
